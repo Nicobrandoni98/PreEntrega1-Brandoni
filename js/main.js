dd472e4ced2a04f8de3eafaba6e919e8771e563c
Nicobrandoni98 <nico.brandoni98@gmail.com>
2023-05-17 21:06:01 -0300

    4to commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let mensaje = "Nombres y fechas de cumpleaños:\n";
 for (let i = 1; i <= 5; i++) {
   let nombre = prompt("Ingrese el nombre " + i + ":");
   let fechaCumple = prompt("Ingrese la fecha de cumpleaños de " + nombre + ":");
+  console.log("Nombres y fechas");
 
   mensaje += nombre + ": " + fechaCumple + "\n";
 }
